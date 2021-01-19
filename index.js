5cb748146a6238e685a9a886b691448e32333098
peacepiece7 <73880776+peacepiece7@users.noreply.github.com>
2021-01-19 21:23:33 +0900

    test1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hellow')
+const express = require("express");
+// appication = app 에 express()를 실행시킴
+const app = express();
+//connect app(application) with 3000 port
+const PORT = 3000;
+function handleListening(){
+    console.log(`listening on: http://localhost:${PORT}`);
+}
+function handleHome(request,responsive){
+    console.log(request)
+    responsive.send('hellow')
+}
+app.get('/',handleHome)
+
+app.listen(PORT, handleListening);
