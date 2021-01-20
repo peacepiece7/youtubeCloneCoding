00d592b4aa4f2d937036dc79ae6049d2607db521
peacepiece7 <73880776+peacepiece7@users.noreply.github.com>
2021-01-21 02:39:46 +0900

    import babel, helmet etc

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require("express");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
 // appication = app 에 express()를 실행시킴
-const app = express();
+const app=express();
 //connect app(application) with 3000 port
-const PORT = 3000;
-function handleListening(){
-    console.log(`listening on: http://localhost:${PORT}`);
-}
-function handleHome(request,responsive){
-    console.log(request)
-    responsive.send('hellow this page is home')
-}
+const PORT=3000;
+const handleListening=()=>console.log(`listening on: http://localhost:${PORT}`);
 
-function handleProfile(req,res){
-    console.log(req);
-    res.send('hellow from profile')
+const handleHome=(request,responsive)=>{
+    console.log(request)
+    responsive.send('hellow this page is my ass')
 }
-app.get('/',handleHome)
-app.get('/prpfile',handleProfile)
+// arrow function 이라고함 function쓰지말고 이거 쓰면 되는 듯
+const handleProfile=(req,res)=>res.send('hellow from profile')
+app.use(morgan("dev"));
+app.get(helmet())
+app.get('/',handleHome);
+app.get('/prfile',handleProfile);
 
 app.listen(PORT, handleListening);
